05b806513bdd82060559f88e59140832252a0b61
Fernand <fernandgit09@gmail.com>
2024-12-20 02:11:07 +0700

    update snackbar untuk connection

--- lib/app/modules/connection/controllers/connection_controller.dart
+++ lib/app/modules/connection/controllers/connection_controller.dart
@@ -1,7 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import 'package:latihan/app/views/home_view.dart';
-
 import '../views/no_connection_view.dart';
 
 class ConnectionController extends GetxController {
@@ -12,19 +12,50 @@ class ConnectionController extends GetxController {
     super.onInit();
     _connectivity.onConnectivityChanged.listen((connectivityResults) {
       // Jika connectivityResults adalah List<ConnectivityResult>, kita ambil hasil pertama
-      _updateConnectionStatus(connectivityResults.first);
+      final connectivityResult = connectivityResults.first;
+
+      // Update koneksi dan navigasi
+      _updateConnectionStatus(connectivityResult);
+
+      // Tampilkan Snackbar setelah navigasi selesai
+      Future.delayed(
+        Duration.zero,
+            () => _showSnackbar(connectivityResult),
+      );
     });
   }
+
   // Fungsi untuk mengupdate status koneksi
   void _updateConnectionStatus(ConnectivityResult connectivityResult) {
-    // kondisi dimana aplikasi mendeteksi bawha tidak ada koneksi sama sekali
     if (connectivityResult == ConnectivityResult.none) {
       Get.offAll(() => const NoConnectionView());
     } else {
-      // else merupakan kondisi jika aplikasi terhubung dengan koneksi wifi atau mobile data
       if (Get.currentRoute == '/NoConnectionView') {
         Get.offAll(() => HomeView());
       }
+    }
+  }
+
+  // Fungsi untuk menampilkan Snackbar
+  void _showSnackbar(ConnectivityResult connectivityResult) {
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.snackbar(
+        'Koneksi Hilang',
+        'Tidak ada internet',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        duration: const Duration(seconds: 3),
+      );
+    } else {
+      Get.snackbar(
+        'Koneksi kembali',
+        'Berhasil Terhubung ke internet',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+        duration: const Duration(seconds: 3),
+      );
     }
   }
 }
